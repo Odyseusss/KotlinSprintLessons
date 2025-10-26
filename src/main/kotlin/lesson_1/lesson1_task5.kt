641090cddcd692c25de72dc09977027093972cc6
Odysseus <brilliantbatman81@gmail.com>
2025-10-26 20:44:12 +0500

    KS-1-5 refactored println, added new const val

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,13 +1,13 @@
 package org.example.lesson_1
 
 fun main() {
-
-    val NUMBER_FOR_CONVERSIONS = 60
     var seconds = 6480
     val minute = (seconds % (NUMBER_FOR_CONVERSIONS * NUMBER_FOR_CONVERSIONS)) / NUMBER_FOR_CONVERSIONS
     val hour = seconds / (NUMBER_FOR_CONVERSIONS * NUMBER_FOR_CONVERSIONS)
     seconds %= NUMBER_FOR_CONVERSIONS
 
-    println(String.format("%02d:%02d:%02d", hour, minute, seconds))
+    println(("%02d:%02d:%02d".format(hour, minute, seconds)))
 
 }
+
+const val NUMBER_FOR_CONVERSIONS = 60
