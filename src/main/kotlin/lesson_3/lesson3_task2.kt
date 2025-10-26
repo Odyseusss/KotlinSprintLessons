65050436c15d994a524b8a3f3793767b13d52db0
Odysseus <brilliantbatman81@gmail.com>
2025-10-26 16:43:22 +0500

    KS-3-2

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var name = "Андреева Татьяна Сергеевна"
+    var age = 20
+
+    println("$name, $age")
+
+    name = "Сидорова Татьяна Сергеевна"
+    age = 22
+
+    println("$name, $age")
+
+}
