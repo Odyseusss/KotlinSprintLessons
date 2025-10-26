8713fb0f0e85843467688365860cb6c9435e998e
Odysseus <brilliantbatman81@gmail.com>
2025-10-26 13:59:00 +0500

    KS-2-4

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val crystalOre = 7
+    val ironOre = 11
+    val buff = 20
+
+    println((crystalOre * buff) / 100)
+    println((ironOre * buff) / 100)
+
+}
